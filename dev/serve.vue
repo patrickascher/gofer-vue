fa39d089acb059dd35914e26911e5b76feba7cdc
Patrick Ascher <pat@fullhouse-productions.com>
2021-02-18 21:46:49 +0100

    init

---
+++ dev/serve.vue
@@ -0,0 +1,20 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { GoferVueSample } from '@/entry.esm';
+import {FccButton} from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    FccButton
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <gofer-vue-sample/>
+    <fcc-button></fcc-button>
+  </div>
+</template>
