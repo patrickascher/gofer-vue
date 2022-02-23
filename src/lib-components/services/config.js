09eb2a2ddb71f7a65a4d1008dce317bf0c0a6648
patrick.ascher <patrick.ascher@dbschenker.com>
2022-02-23 18:06:56 +0100

    added browser language detection

--- src/lib-components/services/config.js
+++ src/lib-components/services/config.js
@@ -8,6 +8,12 @@ export default class config {
         goferConfig = cfg
         http.defaults.baseURL = this.get("webserver.domain")
         i18nService.loadLanguageAsync(_.get(cfg, "webserver.translation.defaultLanguage", "en"), false)
+
+        // loading browser language if exists
+        let language = navigator.languages && navigator.languages[0] || // Chrome / Firefox
+            navigator.language ||   // All browsers
+            navigator.userLanguage; // IE <= 10
+        i18nService.loadLanguageAsync(language.substring(0,2), false)
     }
 
     static get(config = null) {
