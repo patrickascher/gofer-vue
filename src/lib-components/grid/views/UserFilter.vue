97994e7237477114d85b4ae60bbea6a466744a2c
pat@fullhouse-productions.com <10Buck$!gb>
2022-12-19 15:31:51 +0100

    bugfixes

--- src/lib-components/grid/views/UserFilter.vue
+++ src/lib-components/grid/views/UserFilter.vue
@@ -324,6 +324,7 @@ export default {
     deleteField: function (index) {
       this.availableFields.push(this.item.Fields[index])
       this.item.Fields.splice(index, 1)
+      this.calcPosition()
     },
     calcPosition: function () {
       let _this = this
@@ -733,7 +734,7 @@ export default {
                                      v-for="(item,index) in item.Fields">
                           <v-list-item-content>
                             <v-list-item-title class="d-flex flex-row ">
-                              <v-chip small class="d-flex-column">{{ $t(item.Title) }}</v-chip>
+                              <v-chip small class="d-flex-column">{{ $t(item.Title) }} {{item.Title}} {{item.Pos}}</v-chip>
                               <v-spacer/>
                               <v-icon class="d-flex-column justify-end " small @click="deleteField(index)">mdi-delete
                               </v-icon>
